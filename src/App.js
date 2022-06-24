c10870301951e91b294de26c91b400d0337d8dfe
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-06-24 13:53:05 +0300

    tailwind is downloaded to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello Lüto!</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
