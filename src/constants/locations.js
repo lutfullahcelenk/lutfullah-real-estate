8d63e8bdea396b9250846fd3b3dfe804098f4618
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-06-28 15:42:59 +0300

    location constant is added

--- src/constants/locations.js
+++ src/constants/locations.js
@@ -1,3 +1,27 @@
+import ny from "../assets/locations/ny.jpg";
+import california from "../assets/locations/california.jpg";
+import newyork from "../assets/locations/newyork.jpg";
 
-
-export const 
+export const locations = [
+  {
+    id: 1,
+    image: ny,
+    title: "New York",
+    content:
+      "New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that's among the worl's major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park.",
+  },
+  {
+    id: 1,
+    image: california,
+    title: "California",
+    content:
+      "California is a state in the Western United States. California borders Oregon to the north, Nevada and Arizona to the east, the Mexican state of Baja California to the south; and has a coastline along the Pacific Ocean to the west.",
+  },
+  {
+    id: 1,
+    image: newyork,
+    title: "Iskenderun",
+    content:
+      "İskenderun is located on the eastern Mediterranean coast on the Gulf of İskenderun, at the foot of the Amanos Mountains. İskenderun is a busy commercial centre and is one of the largest cities in Hatay Province, similar in size to the provincial seat of Antakya. The city is one of Turkey's largest ports on the Mediterranean and an important industrial centre, home to the İsdemir steelworks, one of Turkey's largest. İskenderun has an active, modern life with good hotels, restaurants and cafes along the palm-lined sea front. ",
+  },
+];
