1aa8e26f400e5145d7f741b4e7659ea88bb6cefd
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-06-28 12:15:52 +0300

    the contents are imported from constants to card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
-const Card = () => {
+const Card = ({ card }) => {
+    console.log('card', card)
   return (
-    <div>Card</div>
-  )
-}
+    <div className="m-3 rounded shadow-lg">
+      <img className="w-full" src={card?.image} alt="place" />
 
-export default Card
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{card?.title}</div>
+        <p className="text-gray-700">{card?.content}</p>
+      </div>
+
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block text-green-300 font-bold text-md px-3 py-1 mr-2 mb-2">$ {card?.price.toFixed(3)}</span>
+      </div>
+
+      <div className="">
+        <span>7 {card?.bedNumber}</span>
+        <span>9 {card?.bathNumber}</span>
+      </div>
+
+      <div>
+        <button>View Property</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
+import { cards } from "../../constants/cards";
 
 const Home = () => {
   return (
@@ -9,8 +10,10 @@ const Home = () => {
       <div className="mt-5 p-10 flex justify-center">
         <h2 className="text-3xl text-gray-500 mb-2">Recent Properties</h2>
       </div>
-      <div className="">
-        <Card />
+      <div className="grid p-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {cards.map((card) => (
+          <Card key={card?.id} card={card} />
+        ))}
       </div>
     </>
   );
