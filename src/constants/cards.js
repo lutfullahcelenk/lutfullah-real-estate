cf984a4b1883c11830a24de5653518e48da797f1
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-06-28 12:15:30 +0300

    created constants for properties

---
+++ src/constants/cards.js
@@ -0,0 +1,36 @@
+import home1 from "../assets/home1.jpg";
+import home2 from "../assets/home2.jpg";
+import home3 from "../assets/home3.jpg";
+
+export const cards = [
+  {
+    id: 1,
+    image: home1,
+    title: "Orchid Villa",
+    content:
+      "Located in the suburbs of Iskenderun, orchid villa offers luxury with a modern tocuh. Equipped with green lighting and a rainwater harvesting system, this property is eco-friendly.",
+    price: 320,
+    bedNumber: 3,
+    bathNumber: 5,
+  },
+  {
+    id: 2,
+    image: home2,
+    title: "1104 Lutfullah",
+    content:
+      "Located in the suburbs of Iskenderun, orchid villa offers luxury with a modern tocuh. Equipped with green lighting and a rainwater harvesting system, this property is eco-friendly.",
+    price: 430,
+    bedNumber: 5,
+    bathNumber: 7,
+  },
+  {
+    id: 3,
+    image: home3,
+    title: "Nexa Epitome",
+    content:
+      "Located in the suburbs of Iskenderun, orchid villa offers luxury with a modern tocuh. Equipped with green lighting and a rainwater harvesting system, this property is eco-friendly.",
+    price: 540,
+    bedNumber: 9,
+    bathNumber: 12,
+  },
+];
