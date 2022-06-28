d0731e1c0d4bf317a8eaa5d0ea3271191a45ca2c
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-06-28 11:39:52 +0300

    created the title of the section and reusable card component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Banner from "../../components/Banner";
+import Card from "../../components/Card";
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <div className="mt-5 p-10 flex justify-center">
+        <h2 className="text-3xl text-gray-500 mb-2">Recent Properties</h2>
+      </div>
+      <div className="">
+        <Card />
+      </div>
     </>
   );
 };
