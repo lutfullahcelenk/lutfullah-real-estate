9f9c6954d5efeb0d1683489aa0c965d10a2cebb0
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-06-29 12:24:12 +0300

    update card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const Card = ({ data }) => {
   return (
-    <div className="rounded overflow-hidden shadow-lg">
+    <div className="overflow-hidden rounded shadow-lg">
       <img className="w-full" src={data?.image} alt="place" />
 
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{data?.title}</div>
+        <div className="mb-2 text-xl font-bold">{data?.title}</div>
         <p className="text-gray-700">{data?.content}</p>
       </div>
 
       {data?.price && (
         <div className="px-6 pt-4 pb-2">
-          <span className="inline-block text-green-300 font-bold text-md px-3 py-1 mr-2">
+          <span className="inline-block px-3 py-1 mr-2 font-bold text-green-300 text-md">
             $ {data?.price.toFixed(3)}
           </span>
         </div>
@@ -20,17 +20,17 @@ const Card = ({ data }) => {
 
       {data?.bedNumber && data?.bathNumber && (
         <div className="px-6 pt-4 pb-2">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-3 mb-2">
+          <span className="inline-block px-3 py-1 mb-2 mr-3 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
             {data?.bedNumber} Beds
           </span>
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-3 mb-2">
+          <span className="inline-block px-3 py-1 mb-2 mr-3 text-sm font-semibold text-gray-700 bg-gray-200 rounded-full">
             {data?.bathNumber} Baths
           </span>
         </div>
       )}
 
       {data?.price && <div className="px-6 pt-4 pb-10">
-        <button className="inline-block bg-green-500 rounded-full px-4 py-1 font-bold text-white mr-3">
+        <button className="inline-block px-4 py-1 mr-3 font-bold text-white bg-green-500 rounded-full">
           View Property
         </button>
       </div>}
