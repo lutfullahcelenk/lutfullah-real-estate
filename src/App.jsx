df0069e476927645c857260744b63551756f1920
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-06-29 16:39:19 +0300

    contact and services pages are created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar"
+//components
+import Navbar from "./components/Navbar";
+//pages
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -11,6 +15,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
